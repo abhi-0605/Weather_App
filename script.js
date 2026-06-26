18f5d62dcf7aeafd80576c950d4b742e2653566c
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-06-26 22:54:07 +0530

    adding js file

---
+++ script.js
@@ -0,0 +1,63 @@
+async function getweather() {
+    const city=document.getElementById("city").value.trim();
+    const result=document.getElementById("result");
+
+    if(city===""){
+        alert("please enter the city")
+        return;
+    }
+    result.innerHTML="Loading..."
+
+    try{
+        const geo_response= await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`);
+        const geo_data= await geo_response.json();
+
+        if(!geo_data.results || geo_data.results.length === 0){
+            result.innerHTML=`<p class="error">city not found</p>`;
+            return;
+        }
+
+        const latitude=geo_data.results[0].latitude;
+        const longitude =geo_data.results[0].longitude;
+        const city_name=geo_data.results[0].name;
+        const country=geo_data.results[0].country;
+
+        const weather_response=await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,wind_direction_10m`);
+        const weather_data=await weather_response.json();
+
+        const weather=weather_data.current;
+
+
+        result.innerHTML=`
+            <div class="weather">
+                <h2>${city_name}, ${country}</h2>
+
+                <p>
+                    Temperature: ${weather.temperature_2m}°C
+                </p>
+
+                <p>
+                    Wind Speed: ${weather.wind_speed_10m} km/h
+                </p>
+
+                <p>
+                    Wind Direction: ${weather.wind_direction_10m}°
+                </p>
+
+                <p>
+                    Time: ${weather.time}
+                </p>
+            </div>
+        `;
+
+
+    }catch(error){
+        result.innerHTML=`
+            <p class="error">
+                failed to fetch weather data
+            </p>
+        `;
+        console.log(error);
+    }
+    
+}
